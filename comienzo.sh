fc6979ec116ba6e34ca983f6cc103749b36c0260
Gentleman.Hu <justfeelingme@gmail.com>
2021-11-10 16:34:46 +0800

    Update comienzo.sh
    remove alive.p;y run

--- comienzo.sh
+++ comienzo.sh
@@ -7,5 +7,5 @@ if [[ -n $ACCOUNTS_ZIP_URL ]]; then
  unzip accounts.zip -d /usr/src/app/accounts
  rm accounts.zip
 fi
-
-gunicorn wserver:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & qbittorrent-nox -d --webui-port=8090 & python3 alive.py & ./aria.sh; python3 -m bot
+# & python3 alive.py
+gunicorn wserver:start_server --bind 0.0.0.0:$PORT --worker-class aiohttp.GunicornWebWorker & qbittorrent-nox -d --webui-port=8090  & ./aria.sh; python3 -m bot
